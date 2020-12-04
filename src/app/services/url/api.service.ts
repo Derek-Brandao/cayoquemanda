afbb8a94939559db8d0543337383c58a7c0a7026
RooseveltSouza <rooseveltsouzav12@gmail.com>
2020-12-03 23:35:58 -0300

    adiciona o AtualizaAnamnese

--- src/app/services/url/api.service.ts
+++ src/app/services/url/api.service.ts
@@ -4,18 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  
+
   //Url do EndPoint
   readonly url: string = 'http://eregular.fabrica.unipe.br:8060/';
-  //readonly url: string = 'http://localhost:5000/';
+  // readonly url: string = 'http://localhost:5000/';
 
-  //Nome dos EndPoints
-  public anamnese = "api/anamneses";
-  public deleteAllAnamneseAluno = "api/anamneses/";
-  public deleteAluno = "api/paluno/";
-  public listarAlunos = "api/alunos";
-  public cadastrarProfessor = "api/professor/cadastrar";
-  public autenticarProfessor = "api/professor/autenticar";
+  // Nome dos EndPoints
+  public anamnese = 'api/anamneses';
+  public deleteAllAnamneseAluno = 'api/anamneses/';
+  public AtualizaAnamnese = 'api/anamnese/';
+  public deleteAluno = 'api/paluno/';
+  public listarAlunos = 'api/alunos';
+  public cadastrarProfessor = 'api/professor/cadastrar';
+  public autenticarProfessor = 'api/professor/autenticar';
 
 
   constructor() { }
